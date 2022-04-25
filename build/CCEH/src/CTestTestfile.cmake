a8e1a6badc9269840b497e0bc3e0fdf88f8cbd08
Chen <ychen.zhong@mavs.uta.edu>
2022-04-25 17:48:32 -0500

    push to github

--- build/CCEH/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhongchen/BufferHashing/CCEH/src
-# Build directory: /home/zhongchen/BufferHashing/build/CCEH/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhongchen/BufferHashing
-# Build directory: /home/zhongchen/BufferHashing/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("CCEH/src")
-subdirs("test")
